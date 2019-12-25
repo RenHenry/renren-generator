66cc3e44725b75e7a5afe257ab7456b265a045a7
RenHenry <RenHenry@users.noreply.github.com>
2019-12-25 15:43:46 +0800

    Update ColumnEntity.java

--- src/main/java/io/renren/entity/ColumnEntity.java
+++ src/main/java/io/renren/entity/ColumnEntity.java
@@ -14,7 +14,6 @@ public class ColumnEntity {
     private String dataType;
     //列名备注
     private String comments;
-    
     //属性名称(第一个字母大写)，如：user_name => UserName
     private String attrName;
     //属性名称(第一个字母小写)，如：user_name => userName
